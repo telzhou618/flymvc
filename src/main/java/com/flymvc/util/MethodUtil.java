bf9efb9e0dc833dad5ffee1f7469e834631345fd
foreyer <274410087@qq.com>
2017-10-17 12:50:49 +0800

    Change-Id: Ie1aa3e6371e12dd6a85d531f42ffbf2c733374d0

--- src/main/java/com/flymvc/util/MethodUtil.java
+++ src/main/java/com/flymvc/util/MethodUtil.java
@@ -4,6 +4,13 @@ import java.lang.reflect.Method;
 
 public class MethodUtil {
 
+	/**
+	 * 根据方法名称获取指定类的第一个方法，不支持重载
+	 * @param controller
+	 * @param methodName
+	 * @return
+	 * @throws NoSuchMethodException
+	 */
 	public static Method getMethod(Class<?> controller, String methodName) throws NoSuchMethodException {
 		// TODO Auto-generated method stub
 		Method[] methods = controller.getMethods();
